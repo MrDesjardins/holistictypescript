8dd4e74ce72f749dab0edba726f85d8cf7c1241d
Patrick Desjardins <mrdesjardins@gmail.com>
2018-02-18 21:13:46 -0800

    Add returns and modify static method

--- classStatic.ts
+++ classStatic.ts
@@ -2,4 +2,7 @@ class StaticClass{
     public static aStaticFunction(){
         console.log("This is called from a static function");
     }
+    public nonStaticFunction(){
+        console.log("Non Static Function");
+    }
 }
---
+++ return.ts
@@ -0,0 +1,6 @@
+function withImplicitReturnType(b:boolean){
+    if(b){
+        return 10;
+    }
+    return "test";
+}
