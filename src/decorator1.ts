693039b19802bcf4889f7ec83d25f70910c7934b
Patrick Desjardins <mrdesjardins@gmail.com>
2018-02-25 21:59:27 -0800

    Minor modification on decorator to have console.log at the decorator level

--- src/decorator1.ts
+++ src/decorator1.ts
@@ -1,20 +1,21 @@
 export function countinstance<T
     extends { new(...args: any[]): {} }>
     (constructor: T) {
+    console.log("This is called once per class type");
     const wrapper = class extends constructor {
         public static instanceCount = 0;
 
         constructor(...args: any[]) {
             super();
             wrapper.instanceCount++;
-            console.log("Count = " + wrapper.instanceCount);
+            console.log("Called every instantiation. Count = " + wrapper.instanceCount);
         }
     }
     return wrapper;
 }
 
-@countinstance 
-class OneClassWithDecorator{
+@countinstance
+class OneClassWithDecorator {
 
 }
 const classDeco1 = new OneClassWithDecorator();
