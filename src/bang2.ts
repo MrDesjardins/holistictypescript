7004ffc06c4ee882462c282013d55f7b48aefc00
Patrick Desjardins <mrdesjardins@gmail.com>
2018-03-02 21:40:15 -0800

    Add a second bang example and numeric seperator examples

---
+++ src/bang2.ts
@@ -0,0 +1,7 @@
+let deferedInitialezVariable!: number[];
+initialize();
+deferedInitialezVariable.push(4);
+
+function initialize(): void {
+    deferedInitialezVariable = [0, 1, 2, 3];
+}
---
+++ src/numericSeparator.ts
@@ -0,0 +1,8 @@
+const numericSeparator1 = 560000067;
+const numericSeparator2 = 560_000_067;
+const numericSeparator3 = 5_6_0_000_0_6_7;
+const numericSeparator4 = Number(5_000);
+const numericSeparator5 = Number("5_000"); // Nan
+const numericSeparator6 = parseInt("5_000"); 
+const numericSeparator7 =  0xFAB_F00D;
+const numericSeparator8 =  0b1111_11111000_11110000_00001100;
